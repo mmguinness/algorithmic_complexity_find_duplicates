39e6af2c27db21c250485552b4fbb20b49c3c101
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-09-16 18:04:30 +0100

    Refactor required to improve quadratic complexity

--- findDuplicates.js
+++ findDuplicates.js
@@ -1,14 +1,18 @@
 const findDuplicates = (array) => {
+  // QUADRATIC COMPLEXITY
+  // for (let i = 0; i < array.length; i++) {
+  //   let wordToCheckAgainst = array[i];
+  //   array[i] = "";
+  //   if (wordToCheckAgainst != "" && array.includes(wordToCheckAgainst)) {
+  //     array[i] = wordToCheckAgainst;
+  //   }
+  // }
+  // return array.filter((word) => word.length > 0);
 
-  for (let i = 0; i < array.length; i++) {
-    let wordToCheckAgainst = array[i];
-    array[i] = "";
-    if (wordToCheckAgainst != "" && array.includes(wordToCheckAgainst)) {
-      array[i] = wordToCheckAgainst;
-    }
-  }
+  // LINEAR COMPLEXITY
+  
 
-  return array.filter((word) => word.length > 0);
+
 };
 
 module.exports = findDuplicates;
